71417e2337498e74fc0a154806623201d80ee180
Alex K <damned182@mail.ru>
2019-05-14 16:33:51 +0300

    add solution for 0-module 1-task

--- 0-module/1-task/sum.js
+++ 0-module/1-task/sum.js
@@ -1,5 +1,7 @@
 function sum(a, b) {
-  /* ваш код */
+  if (typeof a !== 'number' || typeof b !== 'number') throw new TypeError('function sum() should take only numbers');
+
+  return a + b;
 }
 
 module.exports = sum;
