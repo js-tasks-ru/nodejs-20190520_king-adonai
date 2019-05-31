fcf452ea0fd788a02639495c6739e369426aa765
Alex K <damned182@mail.ru>
2019-05-31 16:40:47 +0300

    2 module 1 task

--- 2-module/1-task/LimitSizeStream.js
+++ 2-module/1-task/LimitSizeStream.js
@@ -4,10 +4,21 @@ const LimitExceededError = require('./LimitExceededError');
 class LimitSizeStream extends stream.Transform {
   constructor(options) {
     super(options);
+
+    if (typeof options.limit !== 'undefined') {
+      this.limit = options.limit;
+    }
+    this.total = 0;
   }
 
   _transform(chunk, encoding, callback) {
-
+    this.total += chunk.length;
+    
+    if (this.total > this.limit) {
+      callback(new LimitExceededError());
+    } else {
+      callback(null, chunk);
+    }
   }
 }
 
