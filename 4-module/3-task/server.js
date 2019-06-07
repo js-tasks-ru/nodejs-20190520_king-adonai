7c09280e0ca31addaee676a7077b89c0d626a9d5
Alex K <damned182@mail.ru>
2019-06-07 17:03:55 +0300

    4-module 3-task

--- 4-module/3-task/server.js
+++ 4-module/3-task/server.js
@@ -1,16 +1,36 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
 server.on('request', (req, res) => {
   const pathname = url.parse(req.url).pathname.slice(1);
-
   const filepath = path.join(__dirname, 'files', pathname);
+
+  const dirs = url.parse(req.url).pathname.split('/');
+  if (dirs.length > 2) {
+    res.statusCode = 400;
+    res.end('Nested paths not supported!');
+  }
 
   switch (req.method) {
     case 'DELETE':
+      fs.unlink(filepath, (err) => {
+        if (err) {
+          if (err.code === 'ENOENT') {
+            res.statusCode = 404;
+            res.end('file not found!');
+          } else {
+            res.statusCode = 500;
+            res.end('other error');
+          }
+        } else {
+          res.statusCode = 200;
+          res.end('ok');
+        }
+      });
 
       break;
 
