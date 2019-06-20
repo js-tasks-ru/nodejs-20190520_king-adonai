d6783bc1e02ff68b4b05686dd0b8a38b12e651d6
Alex K <damned182@mail.ru>
2019-06-20 20:14:01 +0300

    6 module 2 task solution

--- 6-module/2-task/app.js
+++ 6-module/2-task/app.js
@@ -1,5 +1,6 @@
 const Koa = require('koa');
 const Router = require('koa-router');
+const User = require('./models/User');
 
 const app = new Koa();
 
@@ -10,12 +11,27 @@ app.use(async (ctx, next) => {
   try {
     await next();
   } catch (err) {
-    if (err.status) {
-      ctx.status = err.status;
-      ctx.body = {error: err.message};
+    if (err.hasOwnProperty('errors')) {
+      const errArr = Object.entries(err.errors);
+      const errs = {};
+
+      for (const [name, { message }] of errArr) {
+        errs[name] = message;
+      };
+
+      ctx.status = 400;
+      ctx.body = { errors: errs };
+    } else if (err.kind === 'ObjectId') {
+      ctx.status = 400;
+      ctx.body = { error: err.message };
     } else {
-      ctx.status = 500;
-      ctx.body = {error: 'Internal server error'};
+      if (err.status) {
+        ctx.status = err.status;
+        ctx.body = { error: err.message };
+      } else {
+        ctx.status = 500;
+        ctx.body = { error: 'Internal server error' };
+      }
     }
   }
 });
@@ -23,23 +39,47 @@ app.use(async (ctx, next) => {
 const router = new Router();
 
 router.get('/users', async (ctx) => {
-
+  ctx.body = await User.find();
 });
 
 router.get('/users/:id', async (ctx) => {
+  const user = await User.findOne({ _id: ctx.params.id });
 
+  if (user) {
+    ctx.body = user;
+  } else {
+    ctx.status = 404;
+    ctx.body = { error: `No user with id: ${ctx.params.id}` };
+  }
 });
 
 router.patch('/users/:id', async (ctx) => {
+  const user = await User.findOneAndUpdate({ _id: ctx.params.id },
+    ctx.request.body,
+    { new: true, runValidators: true });
 
+  if (!user) {
+    ctx.status = 404;
+    ctx.body = { error: `No user with id: ${ctx.params.id}` };
+  }
+
+  ctx.body = user;
 });
 
 router.post('/users', async (ctx) => {
+  const newUser = await User.create({ ...ctx.request.body });
 
+  ctx.body = newUser;
 });
 
 router.delete('/users/:id', async (ctx) => {
+  const removedUser = await User.deleteOne({ _id: ctx.params.id });
 
+  if (removedUser.deletedCount === 1) {
+    ctx.status = 200;
+  } else {
+    ctx.throw(404, 'Not Found');
+  }
 });
 
 app.use(router.routes());
