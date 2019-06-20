4f474472ced12a0a4c9a52fd802cebc8849181eb
Alex K <damned182@mail.ru>
2019-06-20 18:17:48 +0300

    6 module 1 task solution

--- 6-module/1-task/User.js
+++ 6-module/1-task/User.js
@@ -1,5 +1,25 @@
 const mongoose = require('mongoose');
 
-const schema = new mongoose.Schema({});
+const schema = new mongoose.Schema({
+  email: {
+    type: String,
+    trim: true,
+    lowercase: true,
+    required: true,
+    validate: [{
+      validator(value) {
+        return /^[-.\w]+@([\w-]+\.)+[\w-]{2,12}$/.test(value);
+      },
+      message: 'Некорректный email.'
+    }],
+    unique: true
+  },
+  displayName: {
+    type: String,
+    trim: true,
+    required: true,
+    unique: true
+  }
+}, { timestamps: { createdAt: 'created_at' } });
 
 module.exports = mongoose.model('User', schema);
